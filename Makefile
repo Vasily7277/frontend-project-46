e194a636aea17a05bde74dd310bf80d9b665a2b0
Vasiliy7277 <klimovvv@mail.ru>
2022-12-04 17:55:56 +0300

    step 1&2

---
+++ Makefile
@@ -0,0 +1,20 @@
+install:
+	npm ci
+
+gendiff:
+	node bin/gendiff.js
+
+publish:
+	npm publish --dry-run
+
+lint:
+	npx eslint .
+
+lint-fix:
+	npx eslint --fix .
+
+test-coverage:
+	npx jest --coverage
+
+test:
+	npx jest
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import parser from './parsers.js';
+//import getDifference from './getDifference.js';
+//import formatter from './formatters/index.js';
+
+const getAbsolutePath = (filePath) => path.resolve(process.cwd(), filePath).trim();
+const readFile = (filePath) => fs.readFileSync(getAbsolutePath(filePath), 'utf-8');
+const getFormat = (fileName) => path.extname(fileName).slice(1);
+
+const genDiff = (filepath1, filepath2, formatType = 'stylish') => {
+  const fileData1 = readFile(filepath1);
+  const fileData2 = readFile(filepath2);
+
+  const fileFormat1 = getFormat(filepath1);
+  const fileFormat2 = getFormat(filepath2);
+
+  const data1 = parser(fileData1, fileFormat1);
+  const data2 = parser(fileData2, fileFormat2);
+
+  return formatter(getDifference(data1, data2), formatType);
+};
+
+export default genDiff;
